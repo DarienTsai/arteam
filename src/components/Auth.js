b681a00841147b427606a78fe4fbec3be90c8a8e
Darien Tsai <smartdragondt@gmail.com>
2021-02-09 10:26:16 -0800

    cleaning autogen code

---
+++ src/components/Auth.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Auth extends React.Component{
+  constructor(){
+    super();
+    return;
+  }
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
