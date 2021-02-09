d1b0b27842db24587a2ed444f9016c41dcf0abf4
Darien Tsai <smartdragondt@gmail.com>
2021-02-09 10:43:59 -0800

    update readme, more cleanup to come

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Home extends React.Component{
+  constructor(){
+    super();
+    return;
+  }
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
